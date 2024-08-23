86233e47ddd948768b41b95aa3aff0cef1fbd55e
shubham-2122 <shubhamjadav.tops@gmail.com>
2024-08-23 21:04:46 +0530

    code

---
+++ demo/src/Component/Class_compo.js
@@ -0,0 +1,42 @@
+// Component :- Resuse compo :- type :- class  and function 
+// class :- class,constator,super,reder,retrun class exntend 
+// import React, { Component } from "react";
+
+// class Class_compo extends Component{
+//     render(){
+//         return(
+//             <>
+//                 <h1>Hello this is class compo</h1>
+//             </>
+//         )
+//     }
+// }
+
+// export default Class_compo;
+
+// import React, { Component } from 'react'
+
+// export class Class_compo extends Component {
+//   render() {
+//     return (
+//       <div>
+        
+//       </div>
+//     )
+//   }
+// }
+
+// export default Class_compo
+
+import React, { Component } from 'react'
+
+export default class Class_compo extends Component {
+  render() {
+    return (
+      <div>
+        <h1>RCC This is Class compo</h1>
+      </div>
+    )
+  }
+}
+
